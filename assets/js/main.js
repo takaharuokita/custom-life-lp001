9cb01f9bc26aa0bbff91ed4c862fde31fdf464bd
T_okita <t.remolo.okita.0813@gmail.com>
2021-03-14 11:28:08 +0900

    komitto

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+﻿
+$(function(){
+  var setAnim = $('.animate');
+  $(window).on('scroll resize',function(){
+    var setHeight = 300;
+    setAnim.each(function(){
+      var setThis = $(this),
+          setElm = $(this).find('.animate-elm');
+      elmTop = setThis.offset().top,
+      scrTop = $(window).scrollTop(),
+      winHeight = $(window).height();
+      if (scrTop > elmTop - winHeight + setHeight){
+        setElm.addClass('show');
+      }
+    });
+  });
+});
